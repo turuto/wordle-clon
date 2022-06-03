647d6947810d6a68bdba0a4d77e6b9e7f38f3456
Alberto García <turuto@gmail.com>
2022-06-03 20:39:53 +0200

    gameManager

---
+++ js/modules/GameManager.js
@@ -0,0 +1,14 @@
+export class GameManager {
+    constructor() {
+
+    }
+
+    startGame(chosenWord) {
+        console.log(chosenword)
+        this.resetGame();
+    }
+
+    resetGame() {
+
+    }
+}
