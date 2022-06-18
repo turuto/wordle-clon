e2120db053c339b3b407b4b4dd14882c75d42861
Alberto García <turuto@gmail.com>
2022-06-18 19:35:13 +0200

    filtered special keys

--- js/modules/keyboard.js
+++ js/modules/keyboard.js
@@ -4,6 +4,7 @@ export class Keyboard {
     constructor(el, enabled) {
         this.el = el;
         this.enabled = enabled;
+        this.alphabet = KEYBOARD_CONFIG.flat().filter(a => a.length === 1);
         this.init();
     }
 
@@ -51,17 +52,19 @@ export class Keyboard {
     handleKey(key) {
         if (this.enabled) {
             if (key.toUpperCase() === 'ENTER') {
-                console.log('ENTER PRESSED')
+                // console.log('ENTER PRESSED')
                 const sendAttemptEvent = new CustomEvent('attemptFinished');
                 this.el.dispatchEvent(sendAttemptEvent);
             } else if (key.toUpperCase() === 'BACKSPACE') {
-                console.log('BACKSPACE PRESSED')
+                // console.log('BACKSPACE PRESSED')
                 const deleteEvent = new CustomEvent('deleteLetter');
                 this.el.dispatchEvent(deleteEvent);
-            } else {
-                console.log('normal ', key)
+            } else if (this.alphabet.includes(key)) {
+                // console.log('letter pressed ', key)
                 const addLetterEvent = new CustomEvent('addLetter', { detail: key });
                 this.el.dispatchEvent(addLetterEvent);
+            } else {
+                return;
             }
         }
     }
