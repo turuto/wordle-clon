a57017123cc3240507aa21e6d91fb0b7679a8bbd
Alberto García Ariza <turuto@gmail.com>
2023-07-27 21:55:15 +0200

    creates versions of vanilla and vue

---
+++ vue/src/components/TheBoard.vue
@@ -0,0 +1,23 @@
+<template>
+   <main>
+        <p>
+            The Bard
+        </p>
+        <Keyboard></Keyboard>
+    </main>
+</template>
+
+<script setup lang="ts">
+import Keyboard from './keyboard/Keyboard.vue';
+
+</script>
+
+<style scoped>
+main {
+    background: cyan;
+    flex-grow: 1;
+    display: flex;
+    flex-direction: column;
+}
+
+</style>
---
+++ vue/src/components/TheFooter.vue
@@ -0,0 +1,13 @@
+<template>
+    <footer>The Footer</footer>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+footer{
+    background: palegreen;
+}
+</style>
